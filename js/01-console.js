f93f7ab16088567f76f5ae757aa7e526ee52d45d
Lisbon <lisbon@dublin.com>
2023-05-29 17:31:33 +0300

    Comment notation and intron to programming

--- js/01-console.js
+++ js/01-console.js
@@ -6,4 +6,28 @@ console.log(1);
 console.log(2);
 console.log(3);
 
-console.log(4, 5, 6 7);
+console.log(4, 5, 6 7); // need comma before, add comma and hit save/
+
+
+//COMMENT STYLES: /
+
+// Single-line comment: ctrl + /
+
+// Multi-line
+// as
+// single-line
+// comment
+
+/* Single-line as multi-line comment */
+
+/* Single-line
+as
+multi-line
+comment */
+
+/*
+    Single-line
+    as
+    multi-line
+    comment
+*/
