eb67c664527e51d82fcba4f8e7961538ec355cf0
Lisbon <lisbon@dublin.com>
2023-05-31 11:21:28 +0300

    Sting initial samples

--- js/03-stringai.js
+++ js/03-stringai.js
@@ -1,4 +1,38 @@
-// teorija
+/* teorija
+
+STRING - tekstiniu simboliu grandinele, aka tekstas
+Iniciavimo budai:
+- viengubos kabutes ('')
+- dvygubos kabutes ("")
+*/
+
+const vardas = 'Jonas';
+const vardas2 = "Maryte";
+
+console.log(vardas);
+console.log(vardas2)
+
+/*
+//eg.1.1. result: Ona Onaityte (su tarpu)
+
+const firstName = 'Ona ';   // tarpas pries kabutis, taciau nelabai naudotinas!
+const lastName = 'Onaityte';
+const fullName = firstName + lastName;
+console.log(fullName);
+*/
+
+//eg.1.2. result: Ona Onaityte (su tarpu)
+
+const firstName = 'Ona';  // be tarperlio
+const lastName = 'Onaityte';
+const fullName = firstName + ' ' + lastName; // tarpelis su ''
+console.log(fullName);
+
+// Sveiki, as esu Ona!
+// Sveiki, as esu Jonas!
+// Sveiki, as esu Maryte!
+const rekiam = 'Sveiki, as esu ' + firstName + '!'; // musu pvz Ona
+console.log(rekiam);
 
 
 
