9c2bec7e319af6cb19c247f98aabcb15b311bcf5
Lisbon <lisbon@dublin.com>
2023-06-01 08:25:17 +0300

    Boolean theory and excercises

--- js/05-boolean.js
+++ js/05-boolean.js
@@ -1,14 +1,17 @@
 console.clear();
+
 /*
 BOOLEAN - logine reiksme
 true, false
+
 Loginiai operatoriai:
+
 && (and) - ir (true -> jeigu visi yra true)
 || (or) - arba (true -> jeigu bent vienas yra true)
 */
 
-/*
-const isOld = true;
+
+const isOld = true;     // naudojam prie pav is, nes ktp. const old = 'wine'; ?
 console.log(isOld);
 
 const isFunny = false;
@@ -36,12 +39,13 @@ console.log(true);
 
 console.log('--------------');
 
+
 console.log(false || false && true || false && false || true);
 console.log(false && true || false && false || true);
 console.log(false || false && false || true);
 console.log(false && false || true);
 console.log(false || true);
 console.log(true);
-*/
+
 
 
