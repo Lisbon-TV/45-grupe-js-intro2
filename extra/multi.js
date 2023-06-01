1f12404f0086a70189fc739b4133e8714d7e68c1
Lisbon <lisbon@dublin.com>
2023-06-01 09:29:10 +0300

    Import by name - instead of default

--- extra/multi.js
+++ extra/multi.js
@@ -1,6 +1,9 @@
 const multiplier = 3;
 let number = 0;
 
+//gali prideti ir kitus elementus ir juos itraukti:
+const isfunny = true;
+
 const lentele = `
 ${multiplier} * ${++number} = ${multiplier * number}
 ${multiplier} * ${++number} = ${multiplier * number}
@@ -13,4 +16,24 @@ ${multiplier} * ${++number} = ${multiplier * number}
 ${multiplier} * ${++number} = ${multiplier * number}
 ${multiplier} * ${++number} = ${multiplier * number}`;
 
-export default (lentele);
+//export default (lentele); 
+// -> default exportavimas
+
+// -> vardinis exportavimas, viska, ka turiu, t.y. irgi visa lentele
+// -> gali prideti ir kitus elementus per kableli!
+//export {lentele, multiplier};
+// jei daugiau elementuotu, galimas atvaizdavimo budas:
+
+export {
+    lentele,
+    multiplier,
+    number,
+    isfunny};
+
+/* also:
+export {
+    lentele,
+    multiplier,
+    number,
+};
+*/
--- js/06-import.js
+++ js/06-import.js
@@ -3,15 +3,32 @@ console.clear();
 import count from '../extra/number.js';
 import str from '../extra/string.js';
 
-import lentele from '../extra/multi.js';
+//vardinis importavimas, pav 'lentele', turi buti kaip in source faile:
+//gali prie lenteles importuoti ir kitus elementus: multiplier, number, etc.
+import {lentele, multiplier, number, isfunny} from '../extra/multi.js';
+
+// dedault import 
+// import lentele from '../extra/multi.js';
 // import lentele from '../extra/multi.js'; 
-// -> galiu exportuoti pagal savo pav.!
+// -> galiu exportuoti pagal savo pav, kuris turi buti unikalus, 
+// kad per seach susirastumem in vscode!
 // -> tada console.log(multi);
 
 console.log(count);
 console.log(str);
 
+// default import
+// console.log(lentele);
+
+// vadinis importavimas
+
 console.log(lentele);
+// jei nori istraukti tik multiplier reiksme:
+console.log(multiplier);
+// analogiskai ->:
+console.log(number);
+console.log('isfunny', isfunny);
+
 
 
 
