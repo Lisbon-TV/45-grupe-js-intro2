c5671294cdf81f5af21596c94e63964f483a75ca
Lisbon <lisbon@dublin.com>
2023-06-01 09:45:41 +0300

    Matching names in export and import files

--- js/06-import.js
+++ js/06-import.js
@@ -3,9 +3,10 @@ console.clear();
 import count from '../extra/number.js';
 import str from '../extra/string.js';
 
-//vardinis importavimas, pav 'lentele', turi buti kaip in source faile:
-//gali prie lenteles importuoti ir kitus elementus: multiplier, number, etc.
-import {lentele, multiplier, number, isfunny} from '../extra/multi.js';
+// vardinis importavimas, pav 'lentele', turi buti kaip in source faile:
+// gali prie lenteles importuoti ir kitus elementus: multiplier, number, etc.
+
+//import {lentele, multiplier, number, isfunny} from '../extra/multi.js';
 
 // dedault import 
 // import lentele from '../extra/multi.js';
@@ -14,20 +15,27 @@ import {lentele, multiplier, number, isfunny} from '../extra/multi.js';
 // kad per seach susirastumem in vscode!
 // -> tada console.log(multi);
 
+// kai vadiniam importavime padvadinimas 'lentele' sutampa mano pav. faile!:
+const lentele ='Mano lentele NELIESTI!';
+// tada: keiciam: lentele as ... (savo pav.):
+import {lentele as table, multiplier, number, isfunny} from '../extra/multi.js';
+
 console.log(count);
 console.log(str);
 
 // default import
 // console.log(lentele);
 
-// vadinis importavimas
-
-console.log(lentele);
-// jei nori istraukti tik multiplier reiksme:
+/* vadinis importavimas, cia console taip pat:
+*console.log(lentele);
+    jei nori istraukti tik multiplier reiksme:
 console.log(multiplier);
-// analogiskai ->:
+     analogiskai ->:
 console.log(number);
 console.log('isfunny', isfunny);
+*/
+
+console.log(table);
 
 
 
