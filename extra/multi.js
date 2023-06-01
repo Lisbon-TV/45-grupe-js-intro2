7c9f2881a121a20429020e40ff9f28cb97a5e0fc
Lisbon <lisbon@dublin.com>
2023-06-01 09:01:06 +0300

    Import export strings

---
+++ extra/multi.js
@@ -0,0 +1,16 @@
+const multiplier = 3;
+let number = 0;
+
+const lentele = `
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}`;
+
+export default (lentele);
--- js/06-import.js
+++ js/06-import.js
@@ -1,4 +1,17 @@
 console.clear();
 
 import count from '../extra/number.js';
+import str from '../extra/string.js';
+
+import lentele from '../extra/multi.js';
+// import lentele from '../extra/multi.js'; 
+// -> galiu exportuoti pagal savo pav.!
+// -> tada console.log(multi);
+
 console.log(count);
+console.log(str);
+
+console.log(lentele);
+
+
+
