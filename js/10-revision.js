b1e1c1a2a31dc5237350e82d4a75feb54580a665
Lisbon <lisbon@dublin.com>
2023-06-06 09:13:51 +0300

    Exc. "Ar sviecia saule" - revisited

--- js/10-revision.js
+++ js/10-revision.js
@@ -3,3 +3,28 @@ console.clear();
 // adding a emojis: win key + .  --> then choose your emoji!
 const smiley = [':)', '😊'];
 console.log('This is smiley:', smiley);
+
+// Saule sviecia pratimas:
+
+console.log();
+
+
+const arSvieciaSaule = false;
+const arLyja = true;
+let kaDaryti = 'Nezinau!';
+
+if (arSvieciaSaule == true) {
+    if (arLyja == true) {
+    kaDaryti = 'Pasilik namie!';
+} else {
+    kaDaryti = 'Eik pabegioti!';
+}
+} else {
+    if (arLyja == true) {
+        kaDaryti = 'Eik i sporto sale!';
+    } else {
+        kaDaryti = 'Pailsek namie!';
+    }
+}
+
+console.log(kaDaryti);
