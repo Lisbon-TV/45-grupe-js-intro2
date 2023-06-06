47c671704df6934598b2e13fc8bdb5b30253a1cf
Lisbon <lisbon@dublin.com>
2023-06-06 15:27:31 +0300

    For in-class theory and  excercises

--- js/11-for.js
+++ js/11-for.js
@@ -119,3 +119,100 @@ for (let i = 0; i < marks.length; i++) {
  const mean = sumOfMarks / totalMarks;
 
  console.log(`The mean of total marks is  ${mean}`);
+
+ console.log('................');
+
+ // Kiek skaiciu yra teigiamu?
+//                0   1  2   3   4  5   6   7
+const numbers = [10, -7, 5, 77, 13, -9, 0, 14];
+let kiek = 0;
+
+ for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+        if (number >=0) {
+            kiek++;
+    } else {
+        console.log('Nieko nedarau, ness..', number);
+    }
+ }
+
+ console.log('Kiek:', kiek)
+
+ console.clear();
+
+ /* 
+ Kiek yra zodziu, kurie trumpesni, nei "Labas"?
+ Kiek yra zodziu, kurie ilgesni, nei "Labas"?
+ Kiek yra zodziu, kurie yra tokio pat ilgio, nei "Labas"?
+ Pavyzdinis zodis gali buti kintamas. 
+ */
+
+ const dictionary = ['Labas', 'rytas', 'sakau', 'tau', 'mano', 'mielas', 'mieste', 'Ka', 'tu', 'ka', 'vakare'];
+
+ let short = 0;
+ let same = 0;
+ let long = 0;
+ const word = 'Labas';
+ const span = word.length;
+
+ for (let i = 0; i < dictionary.length; i++) {
+    const word = dictionary[i];
+    if (word.length === span) {
+        same++;
+    } else if (word.length < span) {
+        short++;
+    } else {        // else if nebereika, nes jau lieka visi kit
+        long++;     // else reikalingas, nes dara rodys visus zodzius!   
+    }
+ }
+
+ console.log('Shorter words than "Labas" are:', short);
+ console.log('Longer words than "Labas" are:', same);
+ console.log('Words that are same length like "Labas" are:', long);
+
+
+ // arba, kaip klaseje:
+console.log();
+
+let trumpi = 0;
+let toksPat = 0;
+let ilgu = 0;
+const zodis = 'Labas';
+const ilgis = zodis.length;
+
+for (let i = 0; i < dictionary.length; i++) {
+    const word = dictionary[i];
+
+    if (word.length === ilgis) {
+        toksPat++;
+    } else if (word.length < ilgis) {
+        trumpi++;
+    } else {
+        ilgu++;
+    }
+}
+
+console.log('Trumpesniu zodziu:', trumpi);
+console.log('Tokio pat ilgio zodziu:', toksPat);
+console.log('Ilgesniu zodziu:', ilgu);
+
+console.log();
+
+
+// ## UZDUOTIS:
+// Kiek duotas masyvas turi ieskomo skaiciaus atveju (kiekio)?
+// m1:1 -> 3
+// m1:2 -> 0
+// m1:3 -> 0
+// m2:1 -> 1
+// m2:2 -> 2
+// m2:3 -> 1
+// m4:1 -> 5
+// m4:2 -> 3
+// m4:3 -> 2
+
+const masyvas1 = [1, 1, 1];
+const masyvas2 = [1, 2, 2, 3];
+const masyvas3 = [5, 4, 3, 2, 1];
+const masyvas4 = [5, 4, 3, 2, 1, 1, 1, 1, 1, 2, 2, 3];
+
