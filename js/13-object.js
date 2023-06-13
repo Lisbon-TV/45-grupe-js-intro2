100308383b2ba7562898378afee7906a48482e2a
Lisbon <lisbon@dublin.com>
2023-06-13 15:44:59 +0300

    Class notes check

--- js/13-object.js
+++ js/13-object.js
@@ -1,91 +1,52 @@
 console.clear();
 
-const user1 = ['Jonas', 99, true];
-const user2 = ['Maryte', 88, false];
-const user3 = ['Petras', 77, false];
-const user4 = ['Ona', 66, true];
+const users = [
+    ['Jonas', 99, true],
+    ['Maryte', 88, false],
+    ['Petras', 77, false],
+    ['Ona', 66, true],
+    [55, false, 'Kazys'],
+    ['Natalija', true],
+];
 
+const userIndex = 4;
+const user = users[userIndex];
 
-const user1Name = 'Jonas';
-const user1Age = 99;
-const user1IsMarried = true;
+const userName = user[0];
+const userAge = user[1];
+const userIsMarried = user[2];
 
+// // Sveiki, mano vardas yra VARDAS, man yra XX metu ir as VEDES/NEVEDES.
 
-const user2Name = 'Maryte';
-const user2Age = 88;
-const user2IsMarried = false;
+let vedybinisTekstas = '';
+if (!userIsMarried) {
+    vedybinisTekstas = 'ne';
+}
 
-
-const user3Name = 'Petras';
-const user3Age = 77;
-const user3IsMarried = false;
-
-const user4Name = 'Ona';
-const user4Age = 66;
-const user4IsMarried = true;
-
-
-const users = [user1, user2, user3, user4];
-console.log(users);
-
-const users2 = [
-    ['Jonas', 99, true], 
-    user2,
-    user3, 
-    user4];
-console.log(users);
-
-// const userIndex = 0;
-// const user = users[userIndex};
-//     console.log(user);
-
-    const userIndex = 0;
-const user = users[1];
-    console.log(user);
-
-const userName = user1[0];
-const userAge = user1[1];
-const userIsMarried = user1[2];
-
-console.log(userName);
-console.log(userAge);
-console.log(userIsMarried);
-
-
-// Sveiki mano VARDAS yra XX metu ir as esu VEDES/NEVEDES
-
-// let vedybinisTekstas = '';
-
-// if (userIsMarried) {
-//     // vedes
-//     vedybinisTekstas = 'vedes';
-//     } else {
-//         // nevedes
-//     vedybinisTekstas = 'nevedes';
-//     }
-
-// let vedybinisTekstas2 = 'vedes';
-
-// if (!userIsMarried) {
-//     vedybinisTekstas2 = 'nevedes';
-//     } 
-
-let vedybinisTekstas3 = '';   // nesigauna!
-
-    if (!userIsMarried) {
-        vedybinisTekstas3 = 'ne';
-        } 
-
-    
-let hi = `Sveiki mano ${userName} yra ${userAge} metu ir as esu ${vedybinisTekstas3}.`;
+let hi = `Sveiki, mano vardas yra ${userName}, man yra ${userAge} metu ir as ${vedybinisTekstas}vedes.`;
 
 console.log(hi);
 
+console.clear();
 
-// if (userIsMarried) {
-//     hi2
-// }   paskaitoje paziurek... 
+const students = [
+    {
+        name: 'Jonas',
+        age: 99,
+        isMarried: true,
+    },
+    {
+        name: 'Maryte',
+        age: 88,
+        isMarried: false,
+    },
+];
 
-////////
+const studentIndex = 1;
+const student = students[studentIndex];
 
-
+console.log(students);
+console.log(student);
+console.log(student['age']);
+console.log(student['name']);
+console.log(student['isMarried']);
